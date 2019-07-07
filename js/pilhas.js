bcd91981f5aa6567bd93ea2baa8a90e8554ad6c0
Thiago Bianeck <thiagobianeck@gmail.com>
2019-07-07 16:16:23 -0300

    Implementação inicial dos métodos

--- js/pilhas.js
+++ js/pilhas.js
@@ -4,33 +4,40 @@ function Stack() {
 
   this.push = function (element) {
     // adiciona um novo item à pilha
+    items.push(element);
   };
 
   this.pop = function () {
     // remover o item do topo da pilha
+    return items.pop();
   };
 
   this.peek = function () {
     //devolve o elemento que esta no topo da pilha
+    return items[items.length - 1];
   };
 
   this.isEmpty = function () {
     //informar se a pilha está vazia ou não
+    return items.length === 0;
   };
 
 
   this.clear = function () {
     // limpa a pilha
+    items = [];
   };
 
 
   this.size = function () {
     // informar o tamanho da pilha
+    return items.length;
   };
 
 
   this.print = function () {
     // imprime a pilha no console
+    console.log(items.toString());
   }
 
 
