c2d19a24ad78a70facad1cb601b34b592e743461
Thiago Bianeck <thiagobianeck@gmail.com>
2019-07-07 16:07:12 -0300

    Criação da classe de representação de stacks (pilhas)

---
+++ js/pilhas.js
@@ -0,0 +1,37 @@
+function Stack() {
+
+  let items = [];
+
+  this.push = function (element) {
+    // adiciona um novo item à pilha
+  };
+
+  this.pop = function () {
+    // remover o item do topo da pilha
+  };
+
+  this.peek = function () {
+    //devolve o elemento que esta no topo da pilha
+  };
+
+  this.isEmpty = function () {
+    //informar se a pilha está vazia ou não
+  };
+
+
+  this.clear = function () {
+    // limpa a pilha
+  };
+
+
+  this.size = function () {
+    // informar o tamanho da pilha
+  };
+
+
+  this.print = function () {
+    // imprime a pilha no console
+  }
+
+
+}
