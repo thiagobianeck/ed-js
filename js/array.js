d69aeb045fa9df4c2832aeb49a3365c2aad7159b
Thiago Bianeck <thiagobianeck@gmail.com>
2019-07-07 14:53:32 -0300

    Inserindo Elementos

--- js/array.js
+++ js/array.js
@@ -8,38 +8,58 @@
 
 // console.log(avgTemp[3]);
 
-let daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-
-for (let i = 0; i < daysOfWeek.length; i++) {
-  console.log(daysOfWeek[i]);
-}
-
-console.log('---------------------');
-
-daysOfWeek.forEach(function(item) {
-  // console.log(item);
-});
-
-console.log('---------------------');
-
-for (day of daysOfWeek) {
+// let daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+//
+// for (let i = 0; i < daysOfWeek.length; i++) {
+//   console.log(daysOfWeek[i]);
+// }
+//
+// console.log('---------------------');
+//
+// daysOfWeek.forEach(function(item) {
+// console.log(item);
+// });
+//
+// console.log('---------------------');
+//
+// for (day of daysOfWeek) {
   // console.log(day);
-}
-
-console.log('---------------------');
-
-let fibonacci = [0,1,1];
-
-for (let i = 3; i < 20 ; i++) {
-  fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-}
+// }
+//
+// console.log('---------------------');
+//
+// let fibonacci = [0,1,1];
+//
+// for (let i = 3; i < 20 ; i++) {
+//   fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+// }
 
 // console.log(fibonacci);
 
-for (item of fibonacci) {
-  console.log(item);
-}
+// for (item of fibonacci) {
+  // console.log(item);
+// }
 
+
+let numbers = [0,1,2,3,4,5,6,7,8,9];
+
+numbers[numbers.length] = 10;
+numbers[numbers.length] = 11;
+
+numbers.push(12);
+numbers.push(13);
+numbers.push(14);
+numbers.push(15);
+numbers.push(16);
+
+numbers.unshift(-1);
+numbers.unshift(-2);
+numbers.unshift(-3);
+numbers.unshift(-4);
+
+numbers[0] = -40;
+
+console.log(numbers);
 
 
 
